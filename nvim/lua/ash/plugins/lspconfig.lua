2edc0a52071e6d6513269fe4db3378c078594bc1
ISNewton <ashrfalhag.11@gmail.com>
2023-05-16 21:27:14 +0200

    comment Format command for now

--- nvim/lua/ash/plugins/lspconfig.lua
+++ nvim/lua/ash/plugins/lspconfig.lua
@@ -8,6 +8,8 @@ require('mason-lspconfig').setup({ automatic_installation = true })
 -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
+-- vim.api.nvim_create_user_command('Format', vim.lsp.buf.formatting_seq_sync, {})
+
 -- PHP
 require('lspconfig').intelephense.setup({capabilities = capabilities})
 
@@ -32,6 +34,25 @@ require('lspconfig').jsonls.setup({
   },
 })
 
+-- null-ls : linting and formatting
+require('null-ls').setup({
+  sources = {
+    require('null-ls').builtins.diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    require('null-ls').builtins.diagnostics.trail_space.with({ disabled_filetypes = { 'NvimTree' } }),
+    require('null-ls').builtins.formatting.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    require('null-ls').builtins.formatting.prettierd,
+  },
+})
+
+require('mason-null-ls').setup({ automatic_installation = true })
 -- Keymaps
 vim.keymap.set('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>')
 vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
@@ -41,6 +62,12 @@ vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>')
 vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+
+
+-- Commands
+-- vim.api.nvim_create_user_command('Fm', vim.lsp.buf.formatting(), {})
+-- vim.cmd([[command! Format :lua vim.lsp.buf.formatting()]])
+
 
 -- Diagnostic configuration
 vim.diagnostic.config({
